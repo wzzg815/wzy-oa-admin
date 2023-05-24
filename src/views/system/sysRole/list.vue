4eccaeafc53d8837b770e8806dd2162cd8dd1a13
wuzhenyu <3097831895@qq.com>
2023-05-24 09:48:01 +0800

    cs

---
+++ src/views/system/sysRole/list.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="app-container">
+    角色列表
+  </div>
+</template>
+<script>
+import api from '@/api/system/sysRole'
+
+export default {
+    //定义数据模型
+    data(){
+        return{
+
+        }
+    },
+    //页面渲染成功后获取数据
+    created(){
+        this.fetchData();
+    },
+    //定义方法
+    methods:{
+        fetchData(){
+            api.list().then(res=>{
+                console.log(res);
+            })
+        }
+    }
+}
+</script>
